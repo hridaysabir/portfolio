ca5e7dc044435289c294c41bfd7f184a35cd5047
hsabir <sabir.h@northeastern.edu>
2021-10-28 22:18:09 -0400

    First commit, initial setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,11 +16,10 @@ export default function IndexPage() {
       <Seo title="Gatsby Starter for Portfolio Minimal" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
-        <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
         <AboutSection sectionId="about" heading="About Portfolio Minimal" />
-        <InterestsSection sectionId="details" heading="Details" />
-        <ProjectsSection sectionId="features" heading="Built-in Features" />
-        <ContactSection sectionId="github" heading="Issues?" />
+        <InterestsSection sectionId="skills" heading="Skills" />
+        <ProjectsSection sectionId="projects" heading="Projects" />
+        <ContactSection sectionId="github" heading="Contact Me" />
       </Page>
     </>
   );
